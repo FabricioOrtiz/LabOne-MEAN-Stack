c73e28405ddf2e82536a939ed4df3522029f79c7
FabricioOrtiz <fabricio.ortiz.alfaro@gmail.com>
2022-06-17 09:35:05 -0600

    adding mongodb package

--- lab-angular/backend/app.js
+++ lab-angular/backend/app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+//dgFLMmvk9N7oosrV
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", 
